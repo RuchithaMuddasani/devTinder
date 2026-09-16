2db2c8deb4fcdaba4f824378793a78c8955b1205
Ruchitha Muddasani <ruchithamuddasani123@gmail.com>
2026-09-16 10:13:41 +0530

    Multiple Route Handlers

--- src/app.js
+++ src/app.js
@@ -2,29 +2,33 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-    res.send("Hello get server!!")
-})
+app.get("/user", 
+        //Route Handlers ---- many can be added but only one response can be sent to the client (next() is used to pass the control to the next route handler)
 
-app.post("/user", (req, res) => {
-    res.send("Hello post server!!")
-})
-
-app.put("/user", (req, res) => {
-    res.send("Hello put server!!")
-})
-
-app.patch("/user", (req, res) => {
-    res.send("Hello patch server!!")
-})
-
-app.delete("/user", (req, res) => {
-    res.send("Hello delete server!!")
-})
-
-app.use("/test", (req, res) => {
-    res.send("Test server!!")
-})
+    (req, res,next) => {
+    console.log("first route handler");
+    next();
+    },
+    (req,res,next)=>{
+        console.log("second route handler");
+        //res.send("response 2!!");
+        next();
+    },
+    (req,res,next)=>{
+        console.log("Third route handler");
+        //res.send("response 3!!");
+        next();
+    },
+    (req,res,next)=>{
+        console.log("Fourth route handler");
+        //res.send("response 4!!");
+        next();
+    },
+    (req,res)=>{
+        console.log("Fifth route handler");
+        res.send("Response 5!!");
+    }
+)
 
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777..");
