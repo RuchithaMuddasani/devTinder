797d38cfc2eb17a2fa54acd2c3e829194a3e699e
Ruchitha Muddasani <ruchithamuddasani123@gmail.com>
2026-09-18 23:38:39 +0530

    exploring validation library

--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,48 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 //Defining schema
 const userSchema = new mongoose.Schema({
     firstName : {
-        type : String
+        type : String,
+        required : true,
+        minLength : 4,
+        maxLength : 50,
+        lowercase:true
     },
     lastName : {
         type : String
     },
     email : {
-        type : String
+        type : String,
+        required : true,
+        unique : true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Not a valid email.." + value);
+            }
+        },
     },
     password : {
-        type : String
+        type : String,
+        required : true
     },
     age : {
         type : Number
     },
     gender : {
-        type : String
+        type : String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender is not valid!!");
+            }
+        }
+    },
+    },
+    {
+        timestamps : true
     }
-})
+)
 
 const User = mongoose.model("User", userSchema);
 
